26b670b874393a9afe170e035dd294d110ce70ed
Aniket Singh <Anni103@example.com>
2024-10-14 00:53:47 +0530

    first commit

---
+++ src/context/mockData.js/mockFollowers.js
@@ -0,0 +1,22 @@
+export default [
+    {
+    "login": "ShivamKumarBhatt",
+    "id": 115892661,
+    "node_id": "U_kgDOBuhhtQ",
+    "avatar_url": "https://avatars.githubusercontent.com/u/115892661?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/ShivamKumarBhatt",
+    "html_url": "https://github.com/ShivamKumarBhatt",
+    "followers_url": "https://api.github.com/users/ShivamKumarBhatt/followers",
+    "following_url": "https://api.github.com/users/ShivamKumarBhatt/following{/other_user}",
+    "gists_url": "https://api.github.com/users/ShivamKumarBhatt/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/ShivamKumarBhatt/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/ShivamKumarBhatt/subscriptions",
+    "organizations_url": "https://api.github.com/users/ShivamKumarBhatt/orgs",
+    "repos_url": "https://api.github.com/users/ShivamKumarBhatt/repos",
+    "events_url": "https://api.github.com/users/ShivamKumarBhatt/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/ShivamKumarBhatt/received_events",
+    "type": "User",
+    "site_admin": false
+    }
+    ];
